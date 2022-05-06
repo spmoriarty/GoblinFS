62d87d992c59861ea5f77d295782c1aefc4ebb50
Sean Moriarty <spmoriarty1@gmail.com>
2022-05-06 15:49:18 -0700

    The bros are coming together, and being constructed with the brawler

--- app.js
+++ app.js
@@ -2,8 +2,19 @@
 import { renderBros } from "./bros";
 const brosKnocked = document.querySelector('knockedout-list');
 const brawlerHp = document.querySelector('brawler-bottles');
+const brawlerImg = document.querySelector('brawler-img');
+const form = document.querySelector('form');
+const broListEl = document.querySelector('.bross');
 
 // let state
+let brosKnocked = 0;
+let brawlerHealth = 10;
+let bros = [
+  {id: 1 name: 'Broseph' hp:2},
+  {id 2 name: 'Brodude' hp: 1},
+];
+
+let broId = 3;
 
 // set event listeners 
   // get user input
