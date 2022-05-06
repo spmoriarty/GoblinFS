8b4bef478dd65d4023517446793d8f7277b5c530
Sean Moriarty <spmoriarty1@gmail.com>
2022-05-06 15:58:06 -0700

    fleshing out app.js

--- app.js
+++ app.js
@@ -10,12 +10,33 @@ const broListEl = document.querySelector('.bross');
 let brosKnocked = 0;
 let brawlerHealth = 10;
 let bros = [
-  {id: 1 name: 'Broseph' hp:2},
-  {id 2 name: 'Brodude' hp: 1},
+  {id: 1 name: 'Broseph' hp: 2},
+  {id: 2 name: 'Brodude' hp: 1},
 ];
 
 let broId = 3;
 
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const data = new FormData(form);
+
+  const BroName = data.get('bro-name');
+
+  const newBro = {
+    id: currentId,
+    name: BroName,
+    hp: Math.ceil(Math.random() * 3),
+  };
+  currentId++;
+
+  bros.push(newBro);
+
+  displayBros();
+});
+
+function displayBros() {
+  
+}
 // set event listeners 
   // get user input
   // use user input to update state 
